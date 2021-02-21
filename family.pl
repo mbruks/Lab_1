fc3cc5e7404a7dd1c56f0c81e013de208b78d59d
mbruks <68144505+mbruks@users.noreply.github.com>
2021-02-21 19:08:02 +0300

    Update family.pl

--- family.pl
+++ family.pl
@@ -90,3 +90,5 @@ grand_pas(X):-parent(Y,Z),parent(Z,X),man(Y),write(Y),nl,fail.
 
 grand_ma(X,Y):- parent(X,Z),parent(Z,Y),woman(X),!.
 grand_mas(X):-parent(Y,Z),parent(Z,X),woman(Y),write(Y),nl,fail.
+
+grand_pa_and_son(X,Y):-(grand_pa(X,Y),man(Y),!);grand_pa(Y,X),man(X),!.
