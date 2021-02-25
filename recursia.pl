782855b78831a52d7745e1def12481c7f3ef4fc2
mbruks <68144505+mbruks@users.noreply.github.com>
2021-02-25 18:55:24 +0300

    Update recursia.pl

--- recursia.pl
+++ recursia.pl
@@ -5,3 +5,8 @@ max(_,Y,Y).
 maxi(X,Y,X,Z):-X>=Y,X>=Z,!.
 maxi(_,Y,Y,Z):-Y>=Z,!.
 maxi(_,_,Z,Z):-!.
+
+%X-fact первого аргумента
+%рекурсия вверх
+fact_up(0,1):-!.
+fact_up(N,X):-N1 is N-1,fact_up(N1,X1),X is N*X1.
