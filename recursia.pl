c272985def13df05c8ce1fa5d7b3391efc8952b0
mbruks <68144505+mbruks@users.noreply.github.com>
2021-02-25 21:08:32 +0300

    Update recursia.pl

--- recursia.pl
+++ recursia.pl
@@ -16,3 +16,7 @@ fact_up(N,X):-N1 is N-1,fact_up(N1,X1),X is N*X1.
 fact_down(1,CurX,CurX):-!.
 fact_down(N,CurX,X):-N1 is N-1,CurX1 is CurX*N,fact_down(N1,CurX1,X).
 factorial(N,X):-fact_down(N,1,X).
+
+fib_up(1,1):-!.
+fib_up(2,1):-!.
+fib_up(N,X):-N1 is N-1,fib_up(N1,X1),N2 is N-2,fib_up(N2,X2),X is X1+X2.
