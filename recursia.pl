ce8b430b45fd3d7b197fa26fbe2cc0e0c26a766e
mbruks <68144505+mbruks@users.noreply.github.com>
2021-02-26 21:15:29 +0300

    Update recursia.pl

--- recursia.pl
+++ recursia.pl
@@ -38,3 +38,6 @@ max_up(N,X):-N1 is N div 10, max_up(N1,X2), CurX1 is N mod 10, max(CurX1,X2,X) .
 max_down(N,X):-max_down(N,0,X).
 max_down(0,X,X):-!. %когда нет цифр, остановка
 max_down(CurX,Max,X):-CurX1 is CurX mod 10,CurY is CurX div 10, CurY1 is CurY mod 10,max(CurX1,CurY1,C),max(C,Max,U),max_down(CurY,U,X).
+
+sum_up_del3(0,0):-!.
+sum_up_del3(N,X):-N1 is N div 10,sum_up(N1,X1),del3(N,Y),X is X1 + Y.
